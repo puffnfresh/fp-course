6f95132a2b8d94b1d317383dc6ecfa63c6659b83
Brian McKenna <bmckenna@atlassian.com>
2018-04-09 10:22:41 +1000

    Mon Apr  9 10:22:41 AEST 2018

--- projects/TicTacToe/Main.hs
+++ projects/TicTacToe/Main.hs
@@ -1,23 +1,88 @@
 module Main where
 
-import Control.Exception
 import Control.Applicative
+import Control.Exception
+
+data Move1
+  = Move1 Position
+  deriving (Ord, Eq, Show)
+
+data Move2
+  = Move2 Position Move1
+  deriving (Ord, Eq, Show)
+
+data Move3
+  = Move3 Position Move2
+  deriving (Ord, Eq, Show)
+
+data Move4
+  = Move4 Position Move3
+  deriving (Ord, Eq, Show)
+
+data Move5
+  = Move5 Position Move4
+  deriving (Ord, Eq, Show)
+
+data Move6
+  = Move6 Position Move5
+  deriving (Ord, Eq, Show)
+
+data Move7
+  = Move7 Position Move6
+  deriving (Ord, Eq, Show)
+
+data Move8
+  = Move8 Position Move7
+  deriving (Ord, Eq, Show)
+
+data Move9
+  = Move9 Position Move8
+  deriving (Ord, Eq, Show)
+
+class MovePositions m where
+  movePositions :: m -> [Position] -- TODO: Should be NonEmpty
+
+instance MovePositions Move1 where
+  movePositions (Move1 p) =
+    [p]
+
+instance MovePositions Move2 where
+  movePositions (Move2 p m) =
+    p : movePositions m
+
+instance MovePositions Move3 where
+  movePositions (Move3 p m) =
+    p : movePositions m
+
+instance MovePositions Move4 where
+  movePositions (Move4 p m) =
+    p : movePositions m
+
+instance MovePositions Move5 where
+  movePositions (Move5 p m) =
+    p : movePositions m
+
+instance MovePositions Move6 where
+  movePositions (Move6 p m) =
+    p : movePositions m
+
+instance MovePositions Move7 where
+  movePositions (Move7 p m) =
+    p : movePositions m
+
+instance MovePositions Move8 where
+  movePositions (Move8 p m) =
+    p : movePositions m
+
+instance MovePositions Move9 where
+  movePositions (Move9 p m) =
+    p : movePositions m
 
 data Position
   = P1 | P2 | P3
   | P4 | P5 | P6
   | P7 | P8 | P9
   deriving (Eq, Ord, Show, Read)
-
-data Board a
-  = Board a a a
-          a a a
-          a a a
-  deriving (Eq, Ord, Show)
-
-data UnfinishedBoard
-  = UnfinishedBoard (Board (Maybe Player))
-  deriving (Eq, Ord, Show)
 
 data Player
   = X | O
@@ -27,75 +92,31 @@ data Winner
   = Winner Player | Draw
   deriving (Eq, Ord, Show)
 
-data FinishedBoard
-  = FinishedBoard (Board (Maybe Player)) Winner
-  deriving (Eq, Ord, Show)
-
-boardToList :: Board a -> [a]
-boardToList (Board a b c d e f g h i) =
-  [a, b, c, d, e, f, g, h, i]
-
-boardPosition :: Position -> Board a -> a
-boardPosition P1 (Board a _ _ _ _ _ _ _ _) =
-  a
-boardPosition P2 (Board _ a _ _ _ _ _ _ _) =
-  a
-boardPosition P3 (Board _ _ a _ _ _ _ _ _) =
-  a
-boardPosition P4 (Board _ _ _ a _ _ _ _ _) =
-  a
-boardPosition P5 (Board _ _ _ _ a _ _ _ _) =
-  a
-boardPosition P6 (Board _ _ _ _ _ a _ _ _) =
-  a
-boardPosition P7 (Board _ _ _ _ _ _ a _ _) =
-  a
-boardPosition P8 (Board _ _ _ _ _ _ _ a _) =
-  a
-boardPosition P9 (Board _ _ _ _ _ _ _ _ a) =
-  a
-
-setBoardPosition :: Position -> a -> Board a -> Board a
-setBoardPosition P1 a (Board _ b c d e f g h i) =
-  Board a b c d e f g h i
-setBoardPosition P2 b (Board a _ c d e f g h i) =
-  Board a b c d e f g h i
-setBoardPosition P3 c (Board a b _ d e f g h i) =
-  Board a b c d e f g h i
-setBoardPosition P4 d (Board a b c _ e f g h i) =
-  Board a b c d e f g h i
-setBoardPosition P5 e (Board a b c d _ f g h i) =
-  Board a b c d e f g h i
-setBoardPosition P6 f (Board a b c d e _ g h i) =
-  Board a b c d e f g h i
-setBoardPosition P7 g (Board a b c d e f _ h i) =
-  Board a b c d e f g h i
-setBoardPosition P8 h (Board a b c d e f g _ i) =
-  Board a b c d e f g h i
-setBoardPosition P9 i (Board a b c d e f g h _) =
-  Board a b c d e f g h i
-
-data MovedBoard
-  = FinishingMoveBoard FinishedBoard
-  | MoveBoard UnfinishedBoard
-  deriving (Eq, Ord, Show)
-
 data MoveError
   = PositionNonEmpty Player
   deriving (Eq, Ord, Show)
 
-whoseTurn :: Board (Maybe Player) -> Player
-whoseTurn b =
-  if countPlayer O == countPlayer X then X else O
-  where
-    countPlayer p =
-      length $ filter (== Just p) (boardToList b)
+whoseTurn :: MovePositions m => m -> Player
+whoseTurn m =
+  if even (length (movePositions m))
+  then X
+  else O
 
 threeq :: Eq a => a -> a -> a -> Bool
 threeq a b c =
   a == b && b == c
 
-boardWinner :: Board (Maybe Player) -> Maybe Winner
+data WonBoard m
+  = WonBoard m Winner
+  deriving (Eq, Ord, Show)
+
+data CheckedMove m
+  = FinishedBoard (WonBoard m)
+  | UnfinishedBoard m
+  | BadMove MoveError
+  deriving (Eq, Ord, Show)
+
+boardWinner :: MovePositions m => m -> Maybe Winner
 boardWinner b =
   w P1 P2 P3
   <|> w P1 P4 P7
@@ -105,57 +126,63 @@ boardWinner b =
   <|> w P7 P8 P9
   <|> w P1 P5 P9
   <|> w P7 P5 P3
-  <|> if null $ filter (== Nothing) (boardToList b)
+  <|> if length ps == 9
       then Just Draw
       else Nothing
   where
+    ps =
+      toPlayers b
     w x y z = do
-      x' <- boardPosition x b
-      y' <- boardPosition y b
-      z' <- boardPosition z b
+      x' <- lookup x ps
+      y' <- lookup y ps
+      z' <- lookup z ps
       if threeq x' y' z'
       then Just $ Winner x'
       else Nothing
 
-move :: Position -> UnfinishedBoard -> Either MoveError MovedBoard
-move p (UnfinishedBoard b) =
-  maybe (Right y) (Left . PositionNonEmpty) $ boardPosition p b
-  where
-    x =
-      setBoardPosition p (Just $ whoseTurn b) b
-    y =
-      maybe (MoveBoard $ UnfinishedBoard x) (FinishingMoveBoard . FinishedBoard x)
-        $ boardWinner x
+toPlayers :: MovePositions m => m -> [(Position, Player)]
+toPlayers m =
+  reverse (zip (reverse (movePositions m)) (cycle [X, O]))
 
-whoWon :: FinishedBoard -> Winner
-whoWon (FinishedBoard _ w) =
+check :: MovePositions m => m -> CheckedMove m
+check m =
+  maybe validMove (BadMove . PositionNonEmpty) (inTail isTaken (toPlayers m))
+  where
+    validMove =
+      maybe (UnfinishedBoard m) (FinishedBoard . WonBoard m) (boardWinner m)
+    isTaken (po, _) xs =
+      lookup po xs
+
+inTail :: (a -> [a] -> Maybe b) -> [a] -> Maybe b
+inTail _ [] =
+  Nothing
+inTail f (x:xs) =
+  f x xs <|> inTail f xs
+
+isDraw :: WonBoard Move9 -> Bool
+isDraw =
+  (== Draw) . whoWon
+
+whoWon :: WonBoard m -> Winner
+whoWon (WonBoard _ w) =
   w
 
-class PlayerAt b where
-  playerAt :: Position -> b -> Maybe Player
+playerAt :: MovePositions m => Position -> m -> Maybe Player
+playerAt p =
+  lookup p . toPlayers
 
-instance PlayerAt UnfinishedBoard where
-  playerAt p (UnfinishedBoard b) =
-    boardPosition p b
-
-instance PlayerAt FinishedBoard where
-  playerAt p (FinishedBoard b _) =
-    boardPosition p b
-
-startingBoard :: UnfinishedBoard
-startingBoard =
-  UnfinishedBoard $ Board Nothing Nothing Nothing
-                          Nothing Nothing Nothing
-                          Nothing Nothing Nothing
-
-printBoard :: UnfinishedBoard -> IO ()
-printBoard (UnfinishedBoard b) = do
-  putStrLn $ take 3 x
-  putStrLn $ take 3 $ drop 3 x
-  putStrLn $ take 3 $ drop 6 x
+printBoard :: MovePositions m => m -> IO ()
+printBoard m = do
+  printRow P1 P2 P3
+  printRow P4 P5 P6
+  printRow P7 P8 P9
   where
-    x =
-      f <$> boardToList b
+    ps =
+      toPlayers m
+    printRow a b c =
+      putStrLn [g a, g b, g c]
+    g =
+      f . flip lookup ps
     f (Just X) =
       'X'
     f (Just O) =
@@ -163,18 +190,30 @@ printBoard (UnfinishedBoard b) = do
     f Nothing =
       '.'
 
-run :: UnfinishedBoard -> IO ()
-run b = do
-  printBoard b
+data MainMoves
+  = MainMoves { mainMoves :: [Position] }
+  deriving (Ord, Eq, Show)
+
+instance MovePositions MainMoves where
+  movePositions (MainMoves ps) =
+    ps
+
+run :: MainMoves -> IO ()
+run m = do
+  printBoard m
   putStr "> "
   p <- try readLn :: IO (Either IOException Position)
-  either (\_ -> run b) (\p' -> either print f $ move p' b) p
+  either (\_ -> run m) (\p' -> f (check (MainMoves (p' : mainMoves m)))) p
   where
-    f (MoveBoard b') =
-      run b'
-    f (FinishingMoveBoard b) =
-      print $ whoWon b
+    f (FinishedBoard (WonBoard m' w)) = do
+      printBoard m'
+      print w
+    f (UnfinishedBoard m') =
+      run m'
+    f (BadMove p) = do
+      putStrLn ("Bad move: " ++ show p)
+      run m
 
 main :: IO ()
 main =
-  run startingBoard
+  run (MainMoves [])
