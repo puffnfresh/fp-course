0c07480257d5863888eba2d385bf7d28bdc235fa
Brian McKenna <bmckenna@atlassian.com>
2019-04-11 14:06:55 +0530

    Thu Apr 11 14:06:55 IST 2019

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,52 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fp ch =
+  putStrLn ("============ " ++ fp) *>
+    putStrLn ch
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles xs =
+  void (sequence (uncurry printFile <$> xs))
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp =
+  (\ch -> (fp, ch)) <$> readFile fp
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles xs =
+  sequence (getFile <$> xs)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp =
+  readFile fp >>= \ch -> getFiles (lines ch) >>= printFiles
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= \args ->
+    case headOr args of
+      a :. Nil ->
+        run a
+      _ ->
+        putStrLn "Bad usage"
 
 ----
 
